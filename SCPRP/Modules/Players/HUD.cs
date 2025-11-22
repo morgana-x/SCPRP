47d00c920caba02357a3417c3e2ce0f7d3440e00
morgana-x <89588301+morgana-x@users.noreply.github.com>
2025-11-22 14:44:40 +1100

    Minor HUD changes

--- SCPRP/Modules/Players/HUD.cs
+++ SCPRP/Modules/Players/HUD.cs
@@ -5,24 +5,27 @@ namespace SCPRP.Modules.Players
 {
     internal class HUD : BaseModule
     {
+        RueI.API.Elements.DynamicElement MainTag;
         public override void Load()
         {
-            
+            MainTag = new RueI.API.Elements.DynamicElement(14f, (x) =>
+            {
+                LabApi.Features.Wrappers.Player p = LabApi.Features.Wrappers.Player.Get(x);
+                if (p == null) return "";
+                return $"         <align=left><color={p.GetJobInfo().HexColour()}>{p.GetJobInfo().Name}</color>   <color=#55ff55>${p.GetMoney()}</color></align>";
+            });
         }
 
         DateTime nextHUD = DateTime.Now;
+
+
         public override void Tick()
         {
             if (DateTime.Now < nextHUD) { return;  }
             nextHUD = DateTime.Now.AddSeconds(0.5f);
             foreach (var p in LabApi.Features.Wrappers.Player.GetAll())
             {
-                string tag = $"         <align=left><color={p.GetJobInfo().HexColour()}>{p.GetJobInfo().Name}</color>   <color=#55ff55>${p.GetMoney()}</color></align>";
-                var moneytag = new RueI.API.Elements.BasicElement(14f, tag);
-
-              //  var bgtag = new RueI.API.Elements.BasicElement(35f,"<align=left><color #44444420><size=50>■</size></color></align>");
-                RueI.API.RueDisplay.Get(p).Show(moneytag, 0.7f);
-               //RueI.API.RueDisplay.Get(p).Show(bgtag, 0.7f);
+               RueI.API.RueDisplay.Get(p).Show(MainTag, 0.6f);
             }
         }
 
